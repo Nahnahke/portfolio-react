7a38b796954568c4814493e1efa422b6a226a6e8
Hannah Ek <ekhannahek@gmail.com>
2023-06-29 21:40:06 +0200

    started on projects section and styled mobile

--- src/Projects/FeaturedProjectsList.js
+++ src/Projects/FeaturedProjectsList.js
@@ -1,12 +1,21 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
+import styled from 'styled-components';
 import { Projects } from './Projects'
 import FeaturedProjects from './FeaturedProjects.js';
+
+const StyledHeader = styled.h1`
+font-size: 72px;
+line-height: 80px;
+margin: 64px 0 64px 0;
+text-align: center;
+color:#137B8A;
+`
 
 const FeaturedProjectsList = () => {
   return (
     <section lang="en-US" title="List of Hannah's featured projects">
-      <h1>Featured Projects</h1>
+      <StyledHeader>Featured Projects</StyledHeader>
       {Projects.map((project, index) => (
         <FeaturedProjects
           key={index}
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,6 +1,14 @@
 export const Projects = [
   {
-    imageSrc: '/images/project-quiz.jpg',
+    imgSrc: '/images/project-quiz.jpg',
+    title: 'Project Quiz',
+    description: 'This was a group project where we built a multiple answer quiz in React Redux. We used selectors to access the Redux state and we dispatched to trigger Redux actions.',
+    languages: ['HTML5', 'CSS3', 'React', 'Redux', 'Styled components', 'Mob-programming'],
+    githubLink: 'https://github.com/Nahnahke/quiz-project',
+    netlifyLink: 'https://the-fact-frenzy-quiz.netlify.app/'
+  },
+  {
+    imgSrc: '/images/project-quiz.jpg',
     title: 'Project Quiz',
     description: 'This was a group project where we built a multiple answer quiz in React Redux. We used selectors to access the Redux state and we dispatched to trigger Redux actions.',
     languages: ['HTML5', 'CSS3', 'React', 'Redux', 'Styled components', 'Mob-programming'],
