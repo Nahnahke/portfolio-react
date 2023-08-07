62e83321c8a0de862e0d17c02baec357e32c4149
Hannah Ek <ekhannahek@gmail.com>
2023-08-07 21:44:19 +0200

    finished updating the information in projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,31 +1,31 @@
 export const Projects = [
   {
     imgSrc: '/images/final-project.png',
-    title: 'Project Homeigo',
-    description: 'This was a group project where we built a realtor homepage. We built our own database, backend and frontend using React, Redux, Node.js and MongoDB.',
-    languages: ['HTML5', 'CSS3', 'React', 'Redux', 'Styled components', 'Mob-programming', 'Node.js', 'Express.js', 'MongoDB'],
+    title: 'Homeigo Realtor Homepage',
+    description: 'This was a group project where we built a realtor homepage. We built our own database, backend and frontend using React, Redux, Node.js and MongoDB. We incorporated a map function and a powerful search function as the main components of the page.',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Redux', 'Styled components', 'Mob-programming', 'Node.js', 'Express.js', 'MongoDB'],
     githubLink: 'https://github.com/Nahnahke/final-project-frontend',
     netlifyLink: 'https://bespoke-sprite-a66c76.netlify.app/'
   },
   {
     imgSrc: '/images/project-authenticator.png',
-    title: 'Project Authenticator',
+    title: 'Authenticator',
     description: 'A pair programming project where we built an authenticator using tokens and storing the user password safely in the database considering security and defensive design.',
-    languages: ['HTML5', 'CSS3', 'React', 'Redux', 'Node.js', 'Express.js', 'Styled components', 'Mob-programming'],
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Redux', 'Node.js', 'Express.js', 'Styled components', 'Mob-programming'],
     githubLink: 'https://github.com/Nahnahke/project-auth',
     netlifyLink: 'https://superb-alfajores-2745c6.netlify.app/login'
   },
   {
     imgSrc: '/images/project-happy-thoughts.jpg',
-    title: 'Project Happy Thoughts',
-    description: 'A backend and frontend project where I built an API where you can both fetch and post data to and from the database. Think of it as a version of Twitter, but with less negativity, and 100% fewer Elon Musks using it.',
-    languages: ['HTML5', 'CSS3', 'React', 'Node.js', 'Express.js'],
-    githubLink: 'https://github.com/Nahnahke/quiz-project',
-    netlifyLink: 'https://the-fact-frenzy-quiz.netlify.app/'
+    title: 'Happy Thoughts',
+    description: 'A backend and frontend project where I built an API where you can both fetch and post data to and from the database, sending "happy thoughts" to the feed. Think of it as a version of Twitter, but with less negativity, and 100% fewer Elon Musks using it.',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Node.js', 'Express.js'],
+    githubLink: 'https://github.com/Nahnahke/project-happy-thoughts-api-database-frontend',
+    netlifyLink: 'https://project-happy-thoughts-frontend.netlify.app/'
   },
   {
     imgSrc: '/images/project-labyrinth.png',
-    title: 'Project Labyrinth',
+    title: 'The Labyrinth',
     description: 'A text based adventure game that allows the user to navigate through a maze provided by a backend in React & Redux. We focused on using thunks and Redux to build the communcation with the backend.',
     languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Redux', 'Styled components', 'Mob-programming', 'Restful-API'],
     githubLink: 'https://github.com/Nahnahke/project-labyrinth',
@@ -33,79 +33,79 @@ export const Projects = [
   },
   {
     imgSrc: '/images/project-todo.png',
-    title: 'Project Todo',
-    description: 'This was a group project where we built a multiple answer quiz in React Redux. We used selectors to access the Redux state and we dispatched to trigger Redux actions.',
-    languages: ['HTML5', 'CSS3', 'React', 'Redux', 'Styled components', 'Mob-programming'],
-    githubLink: 'https://github.com/Nahnahke/quiz-project',
-    netlifyLink: 'https://the-fact-frenzy-quiz.netlify.app/'
+    title: 'To do app in React',
+    description: 'This project is a simple todo-app built in React using Redux. The user can add, delete and check their todos, featured along with the day of the week and the current date.',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Redux'],
+    githubLink: 'https://github.com/Nahnahke/project-todos',
+    netlifyLink: 'https://hannah-project-to-do.netlify.app/'
   },
   {
     imgSrc: '/images/project-quiz.jpg',
-    title: 'Project Quiz',
+    title: 'React quiz',
     description: 'This was a group project where we built a multiple answer quiz in React Redux. We used selectors to access the Redux state and we dispatched to trigger Redux actions.',
-    languages: ['HTML5', 'CSS3', 'React', 'Redux', 'Styled components', 'Mob-programming'],
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Redux', 'Styled components', 'Mob-programming'],
     githubLink: 'https://github.com/Nahnahke/quiz-project',
     netlifyLink: 'https://the-fact-frenzy-quiz.netlify.app/'
   },
   {
     imgSrc: '/images/project-design.png',
-    title: 'Project Design Handover',
-    description: 'This was a group project where we built a multiple answer quiz in React Redux. We used selectors to access the Redux state and we dispatched to trigger Redux actions.',
-    languages: ['HTML5', 'CSS3', 'React', 'Redux', 'Styled components', 'Mob-programming'],
-    githubLink: 'https://github.com/Nahnahke/quiz-project',
-    netlifyLink: 'https://the-fact-frenzy-quiz.netlify.app/'
+    title: 'UX Collaboration design hand-off',
+    description: 'A project where I worked hand in hand with UX designer Hanna Jonsson and experienced a design hand-off. The project was a yoga app landing page where I took to heart what role color, typography and overall hierarchy play in web design.',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React'],
+    githubLink: 'https://github.com/Nahnahke/project-design-handoff',
+    netlifyLink: 'https://jazzy-vacherin-1be84d.netlify.app/'
   },
   {
     imgSrc: '/images/project-movies.jpg',
-    title: 'Project Movies',
-    description: 'This was a group project where we built a multiple answer quiz in React Redux. We used selectors to access the Redux state and we dispatched to trigger Redux actions.',
-    languages: ['HTML5', 'CSS3', 'Responsive Design', 'Mob-programming'],
-    githubLink: 'https://github.com/Nahnahke/project-business-site-',
-    netlifyLink: 'https://the-fact-frenzy-quiz.netlify.app/'
+    title: 'Movie Releases in React',
+    description: 'This was a project where we focused on making React apps with multiple pages using routes. We passed information such as product ID:s and titles in the URL and picked it up in the React router in order to load dynamic content. We worked with fetching from APIs and the combination of useState & useEffect.',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'Responsive Design', 'Mob-programming'],
+    githubLink: 'https://github.com/Nahnahke/project-movies',
+    netlifyLink: 'https://project-movies-hannah-irupe-jonas.netlify.app/'
   },
   {
     imgSrc: '/images/project-survey.jpg',
-    title: 'Project Survey',
-    description: 'This was a project where I worked on learning how to structure webpages using HTML. I worked on styling the page using CSS (Box Model, Grid & Flexbox) and how to make it responsive.',
-    languages: ['HTML5', 'CSS3', 'Responsive Design'],
-    githubLink: 'https://github.com/Nahnahke/project-news-site',
-    netlifyLink: 'https://splendid-paletas-c8ee80.netlify.app/'
+    title: 'E-commerce survey',
+    description: 'With the survey app I worked with form in React using the useState hook. My idea was a follow up e-mail to a customer after they have received their order with a link to this page. I worked on my skills, building a site containing components, the useState hook, form fields and how to connect HTML form inputs to state.',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Responsive Design'],
+    githubLink: 'https://github.com/Nahnahke/project-survey',
+    netlifyLink: 'https://project-survey-hannah-ek.netlify.app/'
   },
   {
     imgSrc: '/images/music-releases.jpg',
-    title: 'Project Music Releases',
-    description: 'This was a project where I worked on learning how to structure webpages using HTML. I worked on styling the page using CSS (Box Model, Grid & Flexbox) and how to make it responsive.',
-    languages: ['HTML5', 'CSS3', 'Responsive Design'],
-    githubLink: 'https://github.com/Nahnahke/project-news-site',
-    netlifyLink: 'https://splendid-paletas-c8ee80.netlify.app/'
+    title: 'Music Releases in React',
+    description: 'In this project I created a site in React displaying music releases using an API from Spotify. I worked on breaking up Javascript code into importable modules, how to write JSX and React components and how to use .map to iterate over arrays and data. I also worked towards honing my skills in React and building a site containing props and components.',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'React', 'Pair-programming'],
+    githubLink: 'https://github.com/Nahnahke/project-music-releases',
+    netlifyLink: 'https://music-releases-hannah-sofia.netlify.app/'
   },
   {
     imgSrc: '/images/weather-app.jpg',
-    title: 'Project Weather App',
-    description: 'This was a project where I worked on learning how to structure webpages using HTML. I worked on styling the page using CSS (Box Model, Grid & Flexbox) and how to make it responsive.',
-    languages: ['HTML5', 'CSS3', 'Responsive Design'],
-    githubLink: 'https://github.com/Nahnahke/project-news-site',
-    netlifyLink: 'https://splendid-paletas-c8ee80.netlify.app/'
+    title: 'Weather app built in Javascript',
+    description: 'This project was all about APIs and fetching external data to our website. It was a pair-programming project and we learnt basic JSON, how to use promises & fetch() in Javascript as well as how to work with the Date() object. I very much enjoyed working in a group on this project and I learnt a lot from my fellow coders!',
+    languages: ['HTML5', 'CSS3', 'Javascript', 'Mob-programming'],
+    githubLink: 'https://github.com/Nahnahke/project-weather-app',
+    netlifyLink: 'https://scintillating-tanuki-2a80a2.netlify.app/'
   },
   {
     imgSrc: '/images/guess-who.jpg',
-    title: 'Project Guess Who',
-    description: 'This was a project where I worked on learning how to structure webpages using HTML. I worked on styling the page using CSS (Box Model, Grid & Flexbox) and how to make it responsive.',
-    languages: ['HTML5', 'CSS3', 'Responsive Design'],
-    githubLink: 'https://github.com/Nahnahke/project-news-site',
-    netlifyLink: 'https://splendid-paletas-c8ee80.netlify.app/'
+    title: 'Guess who game built in Javascript',
+    description: 'The guess who game is a small project using arrays & objects in Javascript as well as the use of array methods such as forEach and filter. I worked on structuring my code in my functions and manipulating the DOM using Javascript. A very fun project!',
+    languages: ['HTML5', 'CSS3', 'Javascript'],
+    githubLink: 'https://github.com/Nahnahke/project-guess-who',
+    netlifyLink: 'https://glittery-sunshine-6998ce.netlify.app/'
   },
   {
     imgSrc: '/images/project-chatbot.png',
-    title: 'Project Chatbot',
-    description: 'This was a project where I worked on learning how to structure webpages using HTML. I worked on styling the page using CSS (Box Model, Grid & Flexbox) and how to make it responsive.',
-    languages: ['HTML5', 'CSS3', 'Responsive Design'],
-    githubLink: 'https://github.com/Nahnahke/project-news-site',
+    title: 'Chatbot built in Javascript',
+    description: 'My first JavaScript project and a small little chatbot serving as a fictional bookshop keeper. I worked on using variables and conditionals in Javascript, some string methods to modify string and understand the control flow and use of conditional statements. I learnt how to use functions in JavaScript and I learnt about different datatypes.',
+    languages: ['HTML5', 'CSS3', 'Javascript'],
+    githubLink: 'https://github.com/Nahnahke/project-chatbot',
     netlifyLink: 'https://tubular-kataifi-ad7e5a.netlify.app/'
   },
   {
     imgSrc: '/images/project-newssite.png',
-    title: 'Project News Site',
+    title: 'Newssite built in HTML & CSS',
     description: 'This was a project where I worked on learning how to structure webpages using HTML. I worked on styling the page using CSS (Box Model, Grid & Flexbox) and how to make it responsive.',
     languages: ['HTML5', 'CSS3', 'Responsive Design'],
     githubLink: 'https://github.com/Nahnahke/project-news-site',
