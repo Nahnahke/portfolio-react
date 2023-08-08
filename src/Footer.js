99236ad81edc1a78d2f503c72b301a5fd609840f
Hannah Ek <ekhannahek@gmail.com>
2023-08-08 07:54:01 +0200

    changed margin on profile container in footer

--- src/Footer.js
+++ src/Footer.js
@@ -52,7 +52,7 @@ const ProfileContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 50px;
-  margin-top: 100px;
+  margin-top: 40px;
 
   @media (min-width: 768px) and (max-width: 1024px) {
     flex-direction: row;
