8f95a262526f881236c5bf45f2a5550a3c1edb31
Hannah Ek <ekhannahek@gmail.com>
2023-08-23 16:01:03 +0200

    changed threshold for intersection observer tech section

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -77,7 +77,7 @@ const Tech = () => {
     const options = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.4
+      threshold: 0.2
     };
 
     const observer = new IntersectionObserver((entries) => {
